dd80d3c13664b4be30be36fc0dcd8aaf6fa41d57
Chibuike Okoye <talk2chibyke@yahoo.com>
2022-08-18 22:42:27 +1000

    Read me is in progress

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Random-password-generator
+A random password generator page, which generates a random password of a certain length (between 8 and 128 characters) which contains a combination of uppercase letters, lowercase letters, numbers and special characters depending on the users choice.
+
+## Challenges
+The first problem i encountered was putting in the values for the numeric variable as numbers as opposed to strings.
+I also encountered difficulties pushing the individual values of a selected character type into a new array. After extensive research and trial and error, i managed to find out that i needed to use the for loop to iterate through a given array to push its indivdual values one at a time into a new array.
+
+## Link to github repository
+https://github.com/Notchibby/Random-password-generator.git
+
+## Link to live url deployment
+
+
+This activity helped solidify my understanding and use of java script.
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var character_type = [];
 const capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "~", "`", "[", "{", "]", "}", "|","\\", ";", ":", "'", '"', ",", "<", ".", ">", "/", "?"];
+var special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "~", " ",  "`", "[", "{", "]", "}", "|","\\", ";", ":", "'", '"', ",", "<", ".", ">", "/", "?"];
 var random_password = [];
 
 
@@ -80,7 +80,7 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
